e94efa05db4e0a8e9579617fa80ac8149dce6597
Shane Burke <67300048+fabio-gustav@users.noreply.github.com>
2025-05-30 13:24:22 -0500

    created camera that follows player

---
+++ ben-nanner's-big-adventure-2/Scenes/test_level.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = $Player
+@onready var camera_2d: Camera2D = $Camera2D
+
+func _process(_delta: float) -> void:
+	if player.position.x > camera_2d.position.x:
+		camera_2d.position.x = player.position.x
