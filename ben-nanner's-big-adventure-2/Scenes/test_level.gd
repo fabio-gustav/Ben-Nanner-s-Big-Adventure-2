b6b4040ba9c31601b6ad98b9797109a623296cca
Shane Burke <67300048+fabio-gustav@users.noreply.github.com>
2025-05-31 19:58:18 -0500

    updates

--- ben-nanner's-big-adventure-2/Scenes/test_level.gd
+++ ben-nanner's-big-adventure-2/Scenes/test_level.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@export var camera_offset:int
+
 @onready var player: CharacterBody2D = $"World Objects/Player"
 @onready var camera_2d: Camera2D = $Camera2D
 
 
 func _process(_delta: float) -> void:
-	if player.position.x > camera_2d.position.x:
-		camera_2d.position.x = player.position.x
+	if player.position.x - camera_offset > camera_2d.position.x:
+		camera_2d.position.x = player.position.x - camera_offset
